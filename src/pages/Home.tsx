c6444698151da75820b2f540dfe700defb8938fe
Filipe Raupp <filipe.raupp@hotmail.com>
2022-08-07 19:02:07 -0300

    removendo imagem temporariamente

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import { LinkButton } from "../components/LinkButton";
-import homeIllustrationImg from "@/assets/images/illustration.png"
+import homeIllustrationImg from "..//assets/images/illustration.png"
 import styles from "../styles/Home.module.css";
 
 export function Home() {
   return (
     <section className={styles.section}>
-      <img className={styles.image} src={homeIllustrationImg} alt="" />
+      <img className={styles.image} src="" alt="" />
       <LinkButton to="/tasks" text="Iniciar" />
     </section>
   );
